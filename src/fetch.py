d5a487f14d52d225ff0e5e9a9bac34dd18bc2161
Shiva Abhishek <107854894+shivaabhishek07@users.noreply.github.com>
2023-07-02 01:00:28 +0530

    Create fetch.py

---
+++ src/fetch.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+url = "https://crates.io/api/v1/crates"
+crate_names = []
+next_page = None
+
+while True:
+    resp = requests.request('GET', url=url)
+    data = resp.json()
+    for crate in data['crates']:
+        crate_names.append(crate['id'])
+    next_page = data['meta']['next_page']
+    if not next_page:
+        break
+    url = f'https://crates.io/api/v1/crates{next_page}'
+    with open('../url.txt', 'a') as f:
+        f.write(f'{url}\n')
+
+with open('../crates.json','w') as f:
+    json.dump(crate_names, f,indent=2)
